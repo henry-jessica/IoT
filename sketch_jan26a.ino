b8dcd401ec8afc798f4b95c6612874a44b805eb7
Jessica Henry <72497559+henry-jessica@users.noreply.github.com>
2021-01-27 00:36:06 +0000

    second commit

--- sketch_jan26a.ino
+++ sketch_jan26a.ino
@@ -7,7 +7,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
    digitalWrite(LED_BUILTIN,HIGH); //comando 
-   delay(1000); 
+   delay(500); 
    digitalWrite(LED_BUILTIN,LOW); 
-   delay(1000); 
+   delay(500); 
 }
